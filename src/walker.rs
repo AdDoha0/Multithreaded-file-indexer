d0057ba98781606c8429fd848299df88fabf0115
AdDoha0 <adamdoxaev@gmail.com>
2025-06-23 14:43:49 +0300

    add fn walk_dir

--- src/walker.rs
+++ src/walker.rs
@@ -1,8 +1,18 @@
 
+
 use std::fs::read_dir;          
 use std::path::PathBuf;          
 use std::sync::mpsc::Sender;
 
-pub fn walk_dir() {
-
+pub fn walk_dir(path: PathBuf, tx: Sender<PathBuf>) {
+    if let Ok(entries) = read_dir(path) {
+        for entry in  entries.flatten() {
+            let path = entry.path();
+            if path.is_dir() {
+                walk_dir(path, tx.clone());
+            } else if path.is_file() {
+                let _ = tx.send(path);
+            }
+        };
+    };
 }
